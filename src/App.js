511c034ed4061bf11eeb3b7b0488b97dd4089864
Bob <bob@sporkdao.com>
2021-02-16 00:31:02 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+import logo from './SporkLogoSVG.svg';
+import cloudButton from './vector-cloud-SVG.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Link
+} from "react-router-dom";
+import { Container as myContainer, Row, Col } from 'react-grid-system';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <header className="App-header">
+        <myContainer>
+            <Row justify="between" debug>
+              <Col>
+              <Link to="/">
+                <div class="container">
+                  <div class="centered">Home</div>
+                  <img border="0" src={cloudButton} width="150" height="150" />
+                  </div> 
+                </Link>
+              </Col>
+              <Col>
+              <Link to="/whitepaper">
+                <div class="container">
+                  <div class="centered">Whitepaper</div>
+                  <img border="0" src={cloudButton} width="150" height="150" />
+                  </div> 
+                </Link>
+              </Col>
+              <Col>
+              <Link to="/farm">
+                <div class="container">
+                  <div class="centered">Farm</div>
+                  <img border="0" src={cloudButton} width="150" height="150" />
+                  </div> 
+                </Link>
+              </Col>
+              <Col>
+              <Link to="/presale">
+                <div class="container">
+                  <div class="centered">Presale</div>
+                  <img border="0" src={cloudButton} width="150" height="150" />
+                  </div> 
+                </Link>
+              </Col>
+            </Row>
+          </myContainer>
+
+          <img src={logo} className="App-logo" alt="logo" />
+
+        </header>
+
+        <body className='App-body'>
+        Coming soon
+   
+        </body>
+      </Router>
+
+
     </div>
   );
 }
