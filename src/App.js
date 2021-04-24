aa0cfde02b7e4504451847ac59f814c965369326
SporkDaoDev <bob@sporkdao.com>
2021-04-24 00:25:39 -0700

    :wrench: handle http to https redirects:

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import myLogo from './SporkLogoSVG.svg';
-import twitterIcon from './008-twitter.svg'; 
-import mediumIcon from './026-medium.svg'; 
+import twitterIcon from './008-twitter.svg';
+import mediumIcon from './026-medium.svg';
 import cloudButton from './vector-cloud-SVG.svg';
-import telegramIcon from './telegram_icon.png'; 
+import telegramIcon from './telegram_icon.png';
+import HttpsRedirect from 'react-https-redirect';
+
 import './App.css';
 import {
   BrowserRouter as Router,
-  Link, 
-  Switch, 
+  Link,
+  Switch,
   Route
 } from "react-router-dom";
 import { Container as myContainer, Row, Col, setConfiguration } from 'react-grid-system';
@@ -18,77 +20,75 @@ function App() {
   setConfiguration({ defaultScreenClass: 'sm', gridColumns: 4, gutterWidth: 140 });
   return (
     <div className="App">
-      <Router>
-        <header className="App-header">
-        <myContainer>
-            <Row justify="between" debug>
-              <Col>
-              <Link to="/">
-                <div class="container">
-                  <div class="centered">HOME</div>
-                  <img border="0" src={cloudButton} width="200" height="200" />
-                  </div> 
-                </Link>
-              </Col>
-              <Col>
-              <Link to={{ pathname: "https://sporkdao.gitbook.io/sporkdao/" }} target="_blank">
-                <div class="container">
-                  <div class="centered">WIKI</div>
-                  <img border="0" src={cloudButton} width="200" height="200" />
-                  </div> 
-                </Link>
-              </Col>
-              <Col>
-              <Link to="/farm">
-                <div class="container">
-                  <div class="centered">FARM</div>
-                  <img border="0" src={cloudButton} width="200" height="200" />
-                  </div> 
-                </Link>
-              </Col>
-              <Col>
-              <Link to="/presale">
-                <div class="container">
-                  <div class="centered">PRESALE</div>
-                  <img border="0" src={cloudButton} width="200" height="200" />
-                  </div> 
-                </Link>
-              </Col>
-              
-            </Row>
-          </myContainer>
-          <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/farm">
-            <Farm />
-          </Route>
-          <Route path="/presale">
-            <Presale />
-          </Route>
-    
-        </Switch>
+      <HttpsRedirect>
+        <Router>
+          <header className="App-header">
+            <myContainer>
+              <Row justify="between" debug>
+                <Col>
+                  <Link to="/">
+                    <div class="container">
+                      <div class="centered">HOME</div>
+                      <img border="0" src={cloudButton} width="200" height="200" />
+                    </div>
+                  </Link>
+                </Col>
+                <Col>
+                  <Link to={{ pathname: "https://sporkdao.gitbook.io/sporkdao/" }} target="_blank">
+                    <div class="container">
+                      <div class="centered">WIKI</div>
+                      <img border="0" src={cloudButton} width="200" height="200" />
+                    </div>
+                  </Link>
+                </Col>
+                <Col>
+                  <Link to="/farm">
+                    <div class="container">
+                      <div class="centered">FARM</div>
+                      <img border="0" src={cloudButton} width="200" height="200" />
+                    </div>
+                  </Link>
+                </Col>
+                <Col>
+                  <Link to="/presale">
+                    <div class="container">
+                      <div class="centered">PRESALE</div>
+                      <img border="0" src={cloudButton} width="200" height="200" />
+                    </div>
+                  </Link>
+                </Col>
 
-        </header>
+              </Row>
+            </myContainer>
+            <Switch>
+              <Route exact path="/">
+                <Home />
+              </Route>
+              <Route path="/farm">
+                <Farm />
+              </Route>
+              <Route path="/presale">
+                <Presale />
+              </Route>
 
-        <body className='App-body'>
-        The Sporky Team All Rights Reserved 
-          <div className ='App-contacts'>
+            </Switch>
 
-            <a href="https://www.twitter.com/sporkdao">
-              <img src={twitterIcon} width="30" height="30"/> 
-            </a> 
-            <a href="https://www.medium.com/sporkdao"><img src={mediumIcon} width="30" height="30" /> </a>
-            <a href="https://t.me/sporkdao">         <img src={telegramIcon} width="30" height="30"/> </a>
-          </div>
-         
-        </body>
+          </header>
 
-       
-      </Router>
+          <body className='App-body'>
+            The Sporky Team All Rights Reserved
+          <div className='App-contacts'>
 
+              <a href="https://www.twitter.com/sporkdao">
+                <img src={twitterIcon} width="30" height="30" />
+              </a>
+              <a href="https://www.medium.com/sporkdao"><img src={mediumIcon} width="30" height="30" /> </a>
+              <a href="https://t.me/sporkdao">         <img src={telegramIcon} width="30" height="30" /> </a>
+            </div>
 
+          </body>
+        </Router>
+      </HttpsRedirect>
     </div>
   );
 }
@@ -97,7 +97,7 @@ function App() {
 function Home() {
   return (
     <div>
-         <img src={myLogo} className="App-logo" alt="logo" />
+      <img src={myLogo} className="App-logo" alt="logo" />
     </div>
   );
 }
